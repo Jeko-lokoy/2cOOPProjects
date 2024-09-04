66df39ddd412ad097f704041d8e86a7db5c97223
unknown <jericomaghanoy10@gmail.com>
2024-09-04 08:17:28 +0800

    First commit

---
+++ firstactivity.php
@@ -0,0 +1,12 @@
+<?php
+/*this activity will give the output of the user's 
+fullname, adress, contact, hobbies and social media.
+*/
+
+    $fullname = "Fullname";
+    $adress = "Adress";
+    $contact = "Contact";
+    $hobbies = "Hobbies";
+    $social_media = "Social Media";
+
+    echo "1. $fullname <br> 2. $adress <br> 3. $contact <br> 4. $hobbies <br> 5. $social_media";
